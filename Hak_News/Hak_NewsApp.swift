d571e8ab9b927ef08e84426eff3ab3914ccf075d
Selin Ilya <IVSelin@mail.ru>
2022-03-16 15:57:09 +0400

    Initial Commit

---
+++ Hak_News/Hak_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hak_NewsApp.swift
+//  Hak_News
+//
+//  Created by Ilya Selin on 16.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Hak_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
